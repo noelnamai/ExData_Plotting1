4f67d7e3a9ca0a5a6011f3e0074d3fa28641361f
noelnamai <noelnamai@fas.harvard.edu>
2014-06-08 10:53:18 -0400

    Update

--- plot3.R
+++ plot3.R
@@ -13,12 +13,12 @@ plot3 <- function() {
     
     max_y <- max(data3)
     plot_colors <- c("black","red","blue")
-    par(mar=c(5.1, 4.1, 4.1, 2.1))  
+    par(mar=c(5.1, 4.1, 4.1, 2.1)) 
     
     plot(data$DateTime, data$Sub_metering_1, type="l", col=plot_colors[1], ylim=c(0,max_y), xlab="", ylab="Energy sub metering")    
     lines(data$DateTime, data$Sub_metering_2, type="l", col=plot_colors[2])    
     lines(data$DateTime, data$Sub_metering_3, type="l", col=plot_colors[3])
-    legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=plot_colors, lwd = 1, border="black")
+    legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=plot_colors, lwd=1, border="black", cex=.64)
     
     dev.copy(png, "plot3.png")
     dev.off()
